56bfead8a3b4ae64f2ff61a4abf06694ba1963f5
Ava Narimani <142108327+Ava-99@users.noreply.github.com>
2023-10-31 03:32:01 +0330

    Add files via upload

---
+++ Questions-6/script.js
@@ -0,0 +1,81 @@
+//Question1
+//  const r = parseFloat(prompt("Enter the amount of radius:"));
+// function area (r){
+//     let resultA = r * r * Math.PI;
+//     return resultA;
+// }
+// let resultOfArea = area(r);
+// console.log(resultOfArea)
+
+
+// function circumference (r){
+//     let resultC =  2 * r * Math.PI;
+//     return resultC;
+// }
+// let resultOfcircumference = circumference (r);
+//  console.log(resultOfcircumference)
+
+
+
+//Question2
+// const number = parseInt(prompt("Enter a number:"));
+// const operator = prompt("Enter one of these symbol. ^ for square of number, *** for cube of number, ** for double of number:");
+// function squareOfNumber (number){
+//     let square = number * number;
+//     return square;
+//  }
+//  function doubleOfNumber (number){
+//     let double = number * 2;
+//     return double;
+// }
+// function cubeOfNumber (number){
+//     let cube = number * number * number;
+//     return cube;
+// }
+// if(operator == "^"){
+//     let resultOfSquare = squareOfNumber(number);
+//     console.log(resultOfSquare);
+// }else if(operator == "**"){
+//     let resultOfDouble = doubleOfNumber(number);
+//     console.log(resultOfDouble);
+// }else if(operator == "***"){
+//     let resultOfCube = cubeOfNumber(number);
+//     console.log(resultOfCube);
+// }else{
+//     alert("Your choice wasn't one of our options.");
+// }
+
+
+
+//Question3
+//How many arguments can a function have?
+//a)one b)two or three c)as many as we want d)none
+
+//THE ANSWER IS OPTION c.
+
+
+
+//Question4
+//What is the result of the code below?
+// squareRootFunc(100)
+// function squareRootFunc(num){
+//     return Math.sqrt(num)
+// }
+//a)NaN 
+//b)squareRootFunce(100)
+//c)10
+//d)10000
+
+//THE ANSWER IS OPTION c.
+
+
+
+//QUESTION5
+//What is the problem with this code?
+// function sum(num1, num2){
+//     return num1 + num2;
+// }
+// sum(5, 9)
+
+//Answer: I added "return" before "num1 + num2".
+
